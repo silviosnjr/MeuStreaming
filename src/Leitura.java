02f6db0f2e3ed93460d04a3aaef3f4ea9068cb50
silviosnjr <silviosnjr@gmail.com>
2024-09-22 23:00:14 -0300

    Código fonte do primeiro projeto

--- src/Leitura.java
+++ src/Leitura.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Leitura {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite seu fimle favorito? ");
+        String filme = leitura.nextLine();
+        System.out.println("Qual o ano de lançamento? ");
+        int anoDeLancamento = leitura.nextInt();
+        System.out.println("Qual sua avaliação para o filme? ");
+        double avaliacao = leitura.nextDouble();
+        System.out.println(filme);
+        System.out.println(anoDeLancamento);
+        System.out.println(avaliacao);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,32 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Esse é o Screen Kids!");
+        System.out.println("Filme: Divertidamente");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //padrão dePascalCase
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = false;
+        double notaDoFilme = 8.1;
+
+        // Precedencia
+        double media = (9.8 + 6.3 + 8.8) / 3;
+        System.out.println(media);
+
+        String sinopse;
+        sinopse = """
+                Divertidamente
+                Pixar
+                Como ocorrem as emoções de uma criança
+                Muito bom!
+                Ano de lançamento
+                """+anoDeLancamento;
+        System.out.println(sinopse);
+
+        int classificacao;
+        classificacao = (int) (media / 2);
+
+
     }
 }
